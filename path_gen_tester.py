62d397a99f152ad3d4525c3982bd37bb062c2e4b
Morgan Tenney <morgan.tenney@gmail.com>
2015-11-24 22:45:34 -0800

    It's gross, but it's a start - finds the path around one clump of black pixels

--- path_gen_tester.py
+++ path_gen_tester.py
@@ -1,12 +1,30 @@
 import sys
 import path_generator as PG
+import Tkinter as tk
 
 def main(argv=None):
     filename = str(raw_input("What image would you like to test? "))
+    if (len(filename) == 0):
+        filename = "demo1.png"
     array = PG.getPixelArrayForFilename(filename)
-    PG.printBlackEnoughArray(array)
+    # PG.printBlackEnoughArray(array)
     array = PG.getBoolArray(array)
-    PG.printPixelArray(array)
+    # PG.printPixelArray(array)
+    vec = PG.getRawPaths(array)
+
+    gFrame = tk.Tk()
+    gFrame.geometry('600x500')
+
+    w = tk.Canvas(gFrame, width=200, height=100)
+    w.pack()
+
+    w.create_rectangle(0, 0, len(array[0])*20, len(array)*20,)
+
+    for edge in vec:
+        w.create_line(edge[0][1]*20, edge[0][0]*20, edge[1][1]*20, edge[1][0]*20)
+
+
+    gFrame.mainloop()
 
 if __name__ == "__main__":
     sys.exit(main())
--- path_generator.py
+++ path_generator.py
@@ -1,7 +1,17 @@
 #This file will contain code for taking in an image and returning a queue of paths for the robot to follow
+# PLEASE TAKE CARE - EVERYTHING IS IN COORDINATES (ROW,COLUMN) --> IN OTHER WORDS, (Y,X)
 from PIL import Image
+import Queue
+import numpy
 
 DARK_VAL = 150
+NORTH = (-1,0)
+EAST = (0,1)
+SOUTH = (1,0)
+WEST = (0,-1)
+TURN_RIGHT = ((True, True),(True, False))
+GO_STRAIGHT = ((True, False),(True, False))
+
 
 def getPixelArrayForFilename(filename):
     image = Image.open(filename, 'r')
@@ -45,3 +55,67 @@ def getBoolArray(array):
         for c in range(0, width):
             boolArray[r][c] = isBlackEnough(array[r][c])
     return boolArray
+
+def getRawPaths(array):
+    vec = []
+    height = len(array)
+    width = len(array[0])
+    for r in range(0, height):
+        for c in range(0, width):
+            if array[r][c]:
+                vec.append(((r,c),(r+1,c)))
+                getNextStep((r+1, c), (r, c), array, vec)
+                return vec #NEED TO INVERT CONTENTS AND CONTINUE, NOT RETURN RIGHT AWAY
+    return vec
+
+def getNextStep(current, start, array, vec):
+    # print vec
+    if current == start:
+        return
+    lastEdge = vec[len(vec)-1]
+    direction = tuple(numpy.subtract(lastEdge[1],lastEdge[0]))
+    square = getSurroundingSquare(lastEdge[1], array, direction)
+    # print printPixelArray(square)
+    if (square == TURN_RIGHT):
+        # turn right
+        print "Turning Right"
+        if direction == NORTH:
+            nextPoint = tuple(numpy.add(current, (0,1)))
+        if direction == EAST:
+            nextPoint = tuple(numpy.add(current, (1,0)))
+        if direction == SOUTH:
+            nextPoint = tuple(numpy.add(current, (0,-1)))
+        if direction == WEST:
+            nextPoint = tuple(numpy.add(current, (-1,0)))
+    elif (square == GO_STRAIGHT):
+        print "Going Straight"
+        nextPoint = tuple(numpy.add(current, direction))
+    else:
+        # go left
+        print "Going Left"
+        if direction == NORTH:
+            nextPoint = tuple(numpy.add(current, (0,-1)))
+        if direction == EAST:
+            nextPoint = tuple(numpy.add(current, (-1,0)))
+        if direction == SOUTH:
+            nextPoint = tuple(numpy.add(current, (0,1)))
+        if direction == WEST:
+            nextPoint = tuple(numpy.add(current, (1,0)))
+    vec.append((current, nextPoint))
+    getNextStep(nextPoint,start,array,vec)
+
+def rotated(array):
+    return tuple(zip(*array[::-1]))
+
+#orients it to always act as if our direction is 'up' for comparison to TURN_RIGHT/GO_STRAIGHT
+def getSurroundingSquare((r,c), array, direction):
+    square = ((array[r-1][c-1], array[r-1][c]),(array[r][c-1], array[r][c]))
+    if direction == NORTH:
+        return square
+    square = rotated(square)
+    if direction == WEST:
+        return square
+    square = rotated(square)
+    if direction == SOUTH:
+        return square
+    return rotated(square)
